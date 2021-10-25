1fbdbd3122f0b3875db07926bd485e432bd0dcf9
NICOLAE MOROȘAN <nicolaemorosan2@gmail.com>
2021-10-25 14:01:05 +0300

    In Lab4 e de fapt ce am facut la Lab3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Morosan_Nicolae_Lab2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private DoughnutMachine myDoughnutMachine;
